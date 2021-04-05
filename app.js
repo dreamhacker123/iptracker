bae739a3001441830f73252d9e5b6ce8a79accd4
rohith <58270557+dreamhacker123@users.noreply.github.com>
2021-04-05 14:45:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function getData(){
             
         result_label.innerHTML=`<b>IP ADDRESS:</b><br><b>IP TYPE:</b><br><b>CONTINENT CODE:</b><br><b>COUNTRY:</b><br><b>SERVICE PROVIDER:</b><br><b>REGION:</b><br>
         <b>CITY:</b><br><b>ZIPCODE:</b><br><b>LATITUDE:</b><br><b>LONGITUDE:</b><br><br><b>FLAG:</b><br>`
-        result_section.innerHTML = `${data.ip}<br>${data.version}<br>${data.continent_code}<br>${data.country_name}<br>
+        result_section.innerHTML = `<p style="word-wrap: break-word;">${data.ip}</p><br>${data.version}<br>${data.continent_code}<br>${data.country_name}<br>
         ${data.org.split(" ",2).join(" ")}<br>${data.region}<br>${data.city}<br>${data.postal}<br>
         ${data.latitude}<br>${data.longitude}<br><img src="https://www.countryflags.io/${data.country_code.toLowerCase()}/flat/64.png"/>`;
         console.log(data);
